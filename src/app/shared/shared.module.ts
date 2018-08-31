ddda2f5b0f812f2c139d9bae316e71e28f1ab655
ronaldlunaramos <ronaldwin79@hotmail.com>
2018-08-31 07:05:28 -0400

    Add modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+        BreadcrumbsComponent,
+        SidebarComponent,
+        NopagefoundComponent
+    ],
+    exports: [
+        HeaderComponent,
+        BreadcrumbsComponent,
+        SidebarComponent,
+        NopagefoundComponent
+    ]
+})
+export class SharedModule { }
