8c82c680a676d882337f5cf13d94873553048b0e
ronaldlunaramos <ronaldwin79@hotmail.com>
2018-08-31 07:04:08 -0400

    Add modules and routes

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
-  templateUrl: './pages.component.html',
-  styles: []
+  templateUrl: './pages.component.html'
 })
 export class PagesComponent implements OnInit {
 
